c8753eb3ac1d676cff5b0f7df59f99fc9835d06f
Leandro de Oliveira Custódio <89105379+LPHBackspace@users.noreply.github.com>
2022-05-09 16:25:08 -0300

    Add files via upload

---
+++ treino/script.js
@@ -0,0 +1,18 @@
+ // calcular aceleração gravitacional
+function ex1(){
+
+ var v0 = document.getElementById('v0').value; // pegar o valor digitado no input
+ const g = 10; // aceleração gravitacional 10m/s^2
+
+ tSubida = v0/g;
+ alturaM = ((v0*v0)/(g*2));
+
+ document.getElementById('vem').innerHTML = "Tempo de subida = " + tSubida + " m/s²"; // Imprimir no p
+ console.log("TEMPO DE SUBIDA = " + tSubida + " m/s²"); // imprimir no console
+ alert("TEMPO DE SUBIDA = " + tSubida + " m/s²"); // imprimir no alert
+
+ document.getElementById('altura').innerHTML = "Altura Máxima = " + alturaM + "m";
+ console.log("ALTURA MÁXIMA = " + alturaM + "m");
+ alert("ALTURA MÁXIMA = " + alturaM + "m");
+
+}
